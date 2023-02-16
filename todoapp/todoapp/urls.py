d41f484b69174b3cda5fd8a9980dd157dc9abef0
Xejb <vladiq-artamonenko@rambler.ru>
2023-02-16 15:52:28 +0300

    added styles and html files

--- todoapp/todoapp/urls.py
+++ todoapp/todoapp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contact/', include('todolist.urls'))
+    path('', include('todolist.urls'))
 ]
--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import ToDo
+
 
 def index(request):
+    todo = ToDo.objects.all()
     return HttpResponse('Helloo o o o o o o o o o o o o o o o o o o o o o o o o')
